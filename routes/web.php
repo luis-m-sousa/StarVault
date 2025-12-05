065041e7abbd22e03b95333539dbed28328582f7
luis-m-sousa <luismigueldesousa2707@gmail.com>
2025-12-04 23:09:44 -0300

    Arquivos do TCC atualizado.

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,6 @@ use App\Http\Controllers\CompararController;
 use App\Models\Simulacao;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 
 Auth::routes();
